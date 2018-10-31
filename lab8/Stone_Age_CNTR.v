e7dcda8f0754c5b7ed2b1f85c104f526b0c2e98e
Victor Delaplaine <vdelaplaines@aol.com>
2018-10-31 13:21:59 -0700

    Update Stone_Age_CNTR.v

--- lab8/Stone_Age_CNTR.v
+++ lab8/Stone_Age_CNTR.v
@@ -2,15 +2,23 @@
 `include "reg_nb.v" 
 `include "OUTPUT_DCDR.v" 
 
+`timescale 1ns/1ps
+
+
 
 
 module Stone_Age_CNTR(  BTN,
 			CLK,
-			Num_in_StoneAge_Binary
+			Num_in_StoneAge_Binary,
+	       Num_in_Binary
 			);
+			
+			
  input  BTN;
  input CLK;
  output  [14:0] Num_in_StoneAge_Binary;
+ 
+ output [3:0] Num_in_Binary;
  
  wire [3:0] y; //y is the output of REG
  wire [3:0] y_plus; //y_plus is the output of inputDCDR
@@ -23,7 +31,8 @@ NS_DCDR inputDCDR(.x1({BTN, y}),
 //
 reg_nb #(.n(4)) REG(.data_in(y_plus),
        			.clk(CLK), 
-			.clr(0), 
+			    .clr(0
+			    ), 
 		       	.ld(1), 
 			.data_out(y)
 ); 
@@ -34,6 +43,6 @@ OUTPUT_DCDR outputDCDR(
 		);
 
 
-
+assign Num_in_Binary = y;
 
 endmodule
