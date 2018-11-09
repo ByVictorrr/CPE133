c5c0a7bd0fc0e0ab1d2e12d4648d97fedc610860
Victor Delaplaine <vdelaplaines@aol.com>
2018-11-09 15:21:58 -0800

    Update main.v

--- lab9/main.v
+++ lab9/main.v
@@ -1,21 +1,12 @@
-
-`timescale 1ns/1ps
-
 `include "../Modules/clk_divider_nbit.v"
 `include "./Stone_Age_CNTR.v"
 `include "Multiplexed_Display.v"
 
-module main(BTN,CLK, seg, an, led
-
-);
+module main(BTN,CLK, seg, an, led);
 
 input BTN, CLK;
-
 output [14:0] led;
-
-
-wire [3:0] y;
-
+wire [3:0] ST;
 output [3:0] an;
 output [7:0] seg;
 
@@ -23,10 +14,10 @@ output [7:0] seg;
 
 
 Stone_Age_CNTR LeftModule(
-			.BTN(BTN),
-			.CLK(CLK), //clk_Reg is a slowed down version
-			.Num_in_StoneAge_Binary(led),
-			.Num_in_Binary(y)
+                        .BTN(BTN),
+                        .CLK(CLK), //clk_Reg is a slowed down version
+                        .ST(ST),
+                        .led_mealy(led)
 
 
 );
@@ -36,15 +27,11 @@ Stone_Age_CNTR LeftModule(
 //faster clock than reg
 Multiplexed_Display Display(
                       .CLK(CLK),
-			    .x(y),
-        		    .seg(seg),
-	     		    .an(an)
+                            .x(ST),
+                            .seg(seg),
+                            .an(an)
 
 );
-
-
-
-
 
 
 
