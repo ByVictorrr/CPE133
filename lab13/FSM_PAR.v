e060c9642fcd809515df8e7749065eb0e8a5dfbb
Victor Delaplaine <vdelaplaines@aol.com>
2018-11-25 08:31:17 +0000

    adll

--- lab13/FSM_PAR.v
+++ lab13/FSM_PAR.v
@@ -24,7 +24,7 @@ module FSM_PAR(input CLK, input BTN, input EQ, output reg CLR ,output reg [1:0] 
     //- next state & present state variables
     reg [1:0] NS, PS; 
     //- bit-level state representations
-    parameter [1:0] st_0=2'b00, st_1=2'b01, st_2=2'b10, st_3 = 2'b11; 
+    parameter [1:0] st_0=2'b00, st_1=2'b01, st_2=2'b10; 
     
 
     //- model the state registers
@@ -39,45 +39,29 @@ module FSM_PAR(input CLK, input BTN, input EQ, output reg CLR ,output reg [1:0] 
        case(PS)
           st_0: //loads data into shift reg
           begin
+	  CTRL = 0;
+	   if(BTN ==1)
+	   begin 
              SEL = 2'b01; //loads in parrallel data
-     	     CLR = 0;
-	     //LD = 0;	     
+     	     CLR = 1;
              NS = st_1; 
+     	    end
+	    else 
+	    begin
+            SEL = 2'b00;
+	    CLR = 0;
+	    NS = st_0;
+            end
      	  end 
-          st_1: //
-          begin 
-          SEL = 2'b10;
-  	  //LD = 1;
+          st_1: //load
+          begin
+	  SEL = 2'b10;
 	  CLR = 0;
-		if(EQ == 1)NS = st_1;
-		else NS = st_2;
-             end   
-             
-          st_2: //hold state
-             begin
-		 SEL = 2'b00;
-	     	 //LD = 1;
-		 CLR = 0;	
-		 NS = st_3; 
-             end
-          st_3: //hold state
-             begin
-		 SEL = 2'b00;
-	     	 //LD = 1;
-		 CLR = 0;
-	 	if(BTN == 0 )
-		 begin 	
-		 CLR = 0;
-		 NS = st_3; 
-            	end
-		else //if button is pressed
-		begin
-		NS = st_0;	
-		end
-	     end
-             
-          default: NS = st_0; 
-            
+	  CTRL = 1;
+	  if(EQ == 1) NS = st_1; 
+	  else NS = st_0;
+  	  end
+	  default: NS = st_0; 
           endcase
       end              
 endmodule
