a3bf34140aa39220a56470be6be229c13db33bdb
Victor Delaplaine <vdelaplaines@aol.com>
2018-11-25 05:36:05 +0000

    aded

--- lab13/SERIAL_PARITY_GEN.v
+++ lab13/SERIAL_PARITY_GEN.v
@@ -1,7 +1,7 @@
 `include "../Modules/clk_divider_nbit.v"
 `include "../Modules/comp_nb.v"
 `include "../Modules/usr_nb.v"
-`include "../Modules/cntr_up_clr_nb.v"
+`include "../Modules/cntr_udr_nb.v"
 `include "./FSM_PAR.v"
 
 module SERIAL_PARITY_GEN(input CLK, input BTN, input [15:0] SW, output [4:0] CNT);
@@ -23,7 +23,7 @@ comp_nb #(.n(16)) shift_reg_output_zero(.a(D_OUT),.b(16'b0000000000000000),.eq(E
 FSM_PAR FSM(.BTN(BTN),.EQ(EQ),.CLR(CLR),.SEL(SEL));
 
 //counts how many events or 1's happens that 
-cntr_up_clr_nb #(.n(5)) EVENT_CNTR(.clk(CLK_SLOWER), .clr(CLR), .up(D_OUT[0]), .ld(LD), .D(0), .count(CNT), .rco(0));
+cntr_udr_nb #(.n(5)) EVENT_CNTR(.clk(CLK_SLOWER), .clr(CLR), .up(D_OUT[0]), .ld(LD), .D(0), .count(CNT), .rco(0));
 
 
 //let parity = 1 (ODD)
