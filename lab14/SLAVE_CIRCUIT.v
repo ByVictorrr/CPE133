8e1b66368cbc2692682ca7455bb9d787e133457c
Victor Delaplaine <vdelaplainess@gmail.com>
2018-11-28 20:47:41 -0800

    Update SLAVE_CIRCUIT.v

--- lab14/SLAVE_CIRCUIT.v
+++ lab14/SLAVE_CIRCUIT.v
@@ -1,24 +1,4 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:  Ratner Surf Designs
-// Engineer:  James Ratner
-// 
-// Create Date: 07/07/2018 08:05:03 AM
-// Design Name: 
-// Module Name: fsm_template
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: Generic FSM model with both Mealy & Moore outputs. 
-//    Note: data widths of state variables are not specified 
-//
-// Dependencies: 
-// 
-// Revision:
-// Revision 1.00 - File Created (07-07-2018) 
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 `include "../Modules/mux_2t1_nb.v"
 `include "../Modules/reg_nb.v"
 `include "../Modules/comp_nb.v"
