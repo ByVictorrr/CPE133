c274b35c14fe7608100392ecacb9cd392b69015c
Victor Delaplaine <vdelaplainess@gmail.com>
2018-11-30 14:08:41 -0800

    Update SLAVE_CIRCUIT.v

--- lab14/SLAVE_CIRCUIT.v
+++ lab14/SLAVE_CIRCUIT.v
@@ -16,7 +16,9 @@ assign ZER = 10'b0000000000;
 //x----
 
 
-usr_nb #(.n(10)) USR_A(.data_in({5'b00000, SW[9],SW[8],SW[7],SW[6],SW[5]}), .dbit(0), .sel(SEL_A), .clk(CLK), .clr(0), .data_out(D_OUT_A));
+usr_nb #(.n(10)) USR_A(
+.data_in({5'b00000, SW[9],SW[8],SW[7],SW[6],SW[5]}), 
+.dbit(0), .sel(SEL_A), .clk(CLK), .clr(0), .data_out(D_OUT_A));
 
 usr_nb #(.n(5)) USR_B(.data_in({SW[4],SW[3],SW[2],SW[1],SW[0]}), .dbit(0), .sel(SEL_B), .clk(CLK), .clr(0), .data_out(D_OUT_B));
 
@@ -26,11 +28,11 @@ mux_2t1_nb #(.n(10)) MUX(.D0(ZER),.D1(D_OUT_A),.SEL(D_OUT_B[0]),.D_OUT(M_OUT));
           .a (D_OUT_B), 
           .b (5'b00000), 
           .eq (EQ), 
-          .gt (0), 
-          .lt (0)
+          .gt (), 
+          .lt ()
  );  
  
-rca_nb #(.n(10)) add(.a(SUM),.b(M_OUT),.cin(0),.sum(RCA_OUT),.co(0));
+rca_nb #(.n(10)) add(.a(SUM),.b(M_OUT),.cin(0),.sum(RCA_OUT),.co());
 
 reg_nb #(.n(10)) MY_REG (
           .data_in  (RCA_OUT), 
