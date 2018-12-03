00c73884e297dc093727207d3231346d60736f07
EsteRubio <44786832+EsteRubio@users.noreply.github.com>
2018-12-03 12:49:42 -0800

    Update SR_Multiplier_5b.v

--- lab14/SR_Multiplier_5b.v
+++ lab14/SR_Multiplier_5b.v
@@ -9,6 +9,7 @@ wire [1:0] SEL_A, SEL_B;
 
 clk_divider_nbit #(.n(20)) slow(.clockin(CLK), .clockout(CLK_S));
   
+// this is the actual multiplication of of the inputs  
 FSM_MULT FSM(
 .CLK(CLK), 
 .BTN(BTN), 
@@ -18,6 +19,7 @@ FSM_MULT FSM(
 .SEL_B(SEL_B),
 .LD(LD));
   
+//this acculmulates  
 SLAVE_CIRCUIT SLAVE(
 .CLK(CLK), 
 .CLR(CLR),  
