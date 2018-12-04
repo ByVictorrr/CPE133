143cae480df11d912080a2c98e2fc51293136a24
Victor Delaplaine <vdelaplaines@aol.com>
2018-12-04 06:28:56 +0000

    final

--- lab12/FSM_4b_SORT.v
+++ lab12/FSM_4b_SORT.v
@@ -44,69 +44,69 @@ module FSM_4b_SORT(input CLK, input BTN, input [2:0] LT, output reg led, output 
        LD  = 4'b0000;
       // LT  = 3'b000; should already assign
        
-       case(PS)
-
-	       
-	       
-	        st_0: //waiting state
+       case(PS)    
+       
+	   st_0: //waiting state
 	   begin
-		   led = 1; //sort has completed
-   	   	if(BTN == 1)
+	   led = 0; // off
+	   LD = 4'b0000;
+	   SEL = 6'b000000;
+   	   if(BTN == 1)
 	   	begin
-	    	CLR = 1; //clear 
-		NS  = st_0; //reset state
+	    CLR = 1; //clear 
+		NS  = st_1; //reset state
 		end
 		
 		else //stay in same state
 		begin
 		CLR = 0;
-		NS  = st_10;
+		NS  = st_0;
 		end
-	   
 	  end 
-          st_1:
-	  begin
-	        led = 0; // sort is starting
+	  
+	  
+       st_1: //load state or initalize state
+	   begin
+	    led = 0; // sort is starting
 	  	SEL = 6'b000000; //selecting x,y,z,w which are all the inital values
 		LD  = 4'b1111; //loads all of the inital values on the next state
 		NS  = st_2; //start the sort
-          end
+        end
 
 	  st_2:
 	  begin
 		CLR = 0;
 	  	led = 0;
 		if(LT[0] == 1'b0) //if arr[0] > arr[1]
-	      	begin
+	     begin
 		//swap(arr[0],arr[1])
-		   SEL = 6'b000011; //SEL[2:0] //Three select signals
-		   
+		 SEL = 6'b000011; //SEL[2:0] //Three select signals
 		//SEL[0] feeds arr[0] to the input of REG[1]
 		//SEL[2:1] feeds arr[1] to the input of REG[0]
 		//Therefore swapping them
 		LD  = 4'b0011; //LD [1:0] 
 		//LD[0] loads arr[1] to the ouput of REG[0] on the next state
 		///LD[1] loads arr[0] to the ouput of REG[1] on the next state
-		NS = st_2;
-			
+		NS = st_3;
 		end
 		else NS = st_3;
 	  end
-          st_3:
-          begin
-		  CLR = 0;
-		  led = 0;
-		if(LT[1] == 1'b0) //if arr[1] > arr[2]
-                begin
-                //swap(arr[1],arr[2])
-                SEL = 6'b010100; //SEL[3:1] //four select signals
+          
+      st_3:
+      begin
+	  CLR = 0;
+	  led = 0;
+      if(LT[1] == 1'b0) //if arr[1] > arr[2]
+       begin
+       //swap(arr[1],arr[2])
+              SEL = 6'b010100; //SEL[3:1] //four select signals
                 //SEL[2:1] feeds arr[1] to the input of REG[2]
                 //SEL[3:2] feeds arr[2] to the input of REG[1]
                 //Therefore swapping them
                 LD  = 4'b0110; //LD [2:1]
                 //LD[1] loads arr[2] to the ouput of REG[1] on the next state
                 ///LD[2] loads arr[1] to the ouput of REG[2] on the next state
-		NS = st_3;
+                NS = st_4;
                 end
                 else NS = st_4;
 	  end
@@ -124,16 +124,17 @@ module FSM_4b_SORT(input CLK, input BTN, input [2:0] LT, output reg led, output 
                 LD  = 4'b1100; //LD [3:2]
                 //LD[2] loads arr[3] to the ouput of REG[2] on the next state
                 ///LD[3] loads arr[2] to the ouput of REG[3] on the next state
-		NS = st_4;
+                NS = st_5;
                 end
                 else NS = st_5; //going to 2nd transversal
-	  end
-	  st_5:
+	           end
+	       
+	       st_5:
        	   begin
 		CLR = 0;
         	led=0;
 		if(LT[0] == 1'b0) //if arr[0] > arr[1]
-	      	begin
+	     begin
 		//swap(arr[0],arr[1])
 		SEL = 6'b000011; //SEL[2:0] //Three select signals
 		//SEL[0] feeds arr[0] to the input of REG[1]
@@ -142,10 +143,11 @@ module FSM_4b_SORT(input CLK, input BTN, input [2:0] LT, output reg led, output 
 		LD  = 4'b0011; //LD [1:0] 
 		//LD[0] loads arr[1] to the ouput of REG[0] on the next state
 		///LD[1] loads arr[0] to the ouput of REG[1] on the next state
-		NS = st_5;
+		NS = st_6;
 		end
 		else NS = st_6;	
 	  end
+          
           st_6:
           begin
 		  CLR = 0;
@@ -160,10 +162,12 @@ module FSM_4b_SORT(input CLK, input BTN, input [2:0] LT, output reg led, output 
                 LD  = 4'b0110; //LD [2:1]
                 //LD[1] loads arr[2] to the ouput of REG[1] on the next state
                 ///LD[2] loads arr[1] to the ouput of REG[2] on the next state
-                NS = st_6;
-		end
+        		NS = st_7;
+        		end
                 else NS = st_7;
-	  end
+	           end
+          
+          
           st_7:
           begin
 		  CLR = 0;
@@ -178,10 +182,12 @@ module FSM_4b_SORT(input CLK, input BTN, input [2:0] LT, output reg led, output 
                 LD  = 4'b1100; //LD [3:2]
                 //LD[2] loads arr[3] to the ouput of REG[2] on the next state
                 ///LD[3] loads arr[2] to the ouput of REG[3] on the next state
-		NS = st_7;
+                NS = st_8;
                 end
                 else NS = st_8; //going to 3rd transversal
-	  end
+                end
+	  
+	  
 	  st_8:
           begin
 		CLR = 0;
@@ -196,10 +202,12 @@ module FSM_4b_SORT(input CLK, input BTN, input [2:0] LT, output reg led, output 
 		LD  = 4'b0011; //LD [1:0] 
 		//LD[0] loads arr[1] to the ouput of REG[0] on the next state
 		///LD[1] loads arr[0] to the ouput of REG[1] on the next state
-		NS = st_8;
+		NS = st_9;
 		end
 		else NS = st_9;
 	  end
+         
+         
           st_9:
           begin
 		  CLR = 0;
@@ -214,14 +222,15 @@ module FSM_4b_SORT(input CLK, input BTN, input [2:0] LT, output reg led, output 
                 LD  = 4'b0110; //LD [2:1]
                 //LD[1] loads arr[2] to the ouput of REG[1] on the next state
                 ///LD[2] loads arr[1] to the ouput of REG[2] on the next state
-		NS = st_9;
+                NS = st_10;
                 end
                 else NS = st_10;
-	  end
+	             end
+         
           st_10:
           begin
 		  CLR = 0;
-		  led = 0;
+		  led = 1; //sort has completed
 		if(LT[2] == 1'b0) //if arr[2] > arr[3]
                 begin
                 //swap(arr[2],arr[3])
@@ -231,13 +240,12 @@ module FSM_4b_SORT(input CLK, input BTN, input [2:0] LT, output reg led, output 
                 //Therefore swapping them
                 LD  = 4'b1100; //LD [3:2]
                 //LD[2] loads arr[3] to the ouput of REG[2] on the next state
-		NS = st_10;
                 ///LD[3] loads arr[2] to the ouput of REG[3] on the next state
+	         NS = st_0;
 	         end
 	        else NS = st_0; //if btn not 1 then stay in same state
 	   end
 	  
-	   
 	   default: NS = st_0;
 	
 	endcase
