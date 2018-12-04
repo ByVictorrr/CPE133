7465e6ec1a4332df975dcffa60591ad225de392d
Victor Delaplaine <vdelaplainess@gmail.com>
2018-12-03 19:42:16 -0800

    Update main.v

--- lab14/main.v
+++ lab14/main.v
@@ -1,6 +1,27 @@
 `include "../Modules/univ_sseg.v"
 `include "SR_Multiplier_5b.v"
 
+
+//////////////////////////////////////////////////////////////////////////////////
+// Company:  VANE
+// Engineer: Victor Delaplaine, Esteban Rubio
+// 
+// Create Date: 11/15/2018 08:05:03 AM
+// Design Name: 
+// Module Name: main
+// Project Name: 
+// Target Devices: Basy3 
+// Tool Versions: 
+// Description: Takes in two 5-bit numbers and multiplies one by another number. 
+// These two 5-bit numbers are represented on the basy3 board by switches.
+// Dependencies: SR_Multiplier_5b.v, univ_sseg.v
+// 
+// Revision:
+// Revision 1.00 - File Created (11-17-2018) 
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module main(input CLK, input BTN, input [9:0] SW, output [7:0] seg, output [3:0] an);
 
 wire [9:0] SUM;
