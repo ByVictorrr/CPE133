d1d185135280c54b878f64dc8871e1cf04e46e1f
Victor Delaplaine <vdelaplainess@gmail.com>
2018-12-03 20:12:43 -0800

    Update main.v

--- lab12/main.v
+++ lab12/main.v
@@ -1,3 +1,6 @@
+`include "Multiplexed_Display.v"
+`include "./SORT_4b.v"
+
 `timescale 1ns / 1ps
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -10,24 +13,40 @@
 //// Project Name: Exp 12
 //// Target Devices: Basy3
 //// Tool Versions: 
-///Description: Takes one external input - BTN. When the BTN is pressed the circuit takes 4 values as sorts them into accending order. It takes the input and runs all the other code.
-//// Dependencies:  Multiplexed_Display.v
+///  Description: Takes one external input - BTN. 
+///  When the BTN is pressed the circuit takes 4 values as sorts them into accending order. 
+///  It takes the 4 4-bit numbers and sorts according to their value. 
+///  On the basy 7-segment display the board displays the the number being sorted. And when done the led indicates.
+//// Dependencies:  Multiplexed_Display.v, SORT_4b.v
 //// Revision:
 //// Revision 0.01 - File Created
 //// Additional Comments:
 ////////////////////////////////////////////////////////////////////////////////////
 
-`include "Multiplexed_Display.v"
-`include "./SORT_4b.v"
 
 module main(input BTN,input CLK,input CLR,input [15:0]SW, output [7:0] seg, output [3:0] an, output led);
 
 wire [3:0] arr_0, arr_1, arr_2, arr_3;
 
-SORT_4b ckt(.BTN(BTN), .SW(SW), .CLK(CLK), .led(led), .arr_0(arr_0), .arr_1(arr_1), .arr_2(arr_2), .arr_3(arr_3)); 
+SORT_4b ckt(
+  .BTN(BTN), 
+  .SW(SW), 
+  .CLK(CLK), 
+  .led(led), 
+  .arr_0(arr_0), 
+  .arr_1(arr_1), 
+  .arr_2(arr_2), 
+  .arr_3(arr_3)); 
 
 //faster clock than reg
-  Multiplexed_Display Display(.CLK(CLK), .arr_0(arr_0), .arr_1(arr_1), .arr_2(arr_2), .arr_3(arr_3), .an(an), .seg(seg));
+  Multiplexed_Display Display(
+    .CLK(CLK), 
+    .arr_0(arr_0), 
+    .arr_1(arr_1), 
+    .arr_2(arr_2), 
+    .arr_3(arr_3), 
+    .an(an), 
+    .seg(seg));
 
 
 endmodule
