8f0bc48c338bdbf6a8cb29fab33ce494ad0fad74
Victor Delaplaine <vdelaplainess@gmail.com>
2018-12-03 20:43:12 -0800

    Update SR_Multiplier_5b.v

--- lab14/SR_Multiplier_5b.v
+++ lab14/SR_Multiplier_5b.v
@@ -2,6 +2,28 @@
 `include "./SLAVE_CIRCUIT.v"
 `include "../Modules/clk_divider_nbit.v"
 
+
+//////////////////////////////////////////////////////////////////////////////////
+// Company:  VANE
+// Engineer: Victor Delaplaine, Esteban Rubio
+// 
+// Create Date: 11/15/2018 08:05:03 AM
+// Design Name: 
+// Module Name: SR_Multiplier_5b
+// Project Name: 
+// Target Devices: Basy3 
+// Tool Versions: 
+// Description: Takes in two 5-bit numbers and multiplies one by another number. 
+// These two 5-bit numbers are represented on the basy3 board by switches.
+// Dependencies: SLAVE_CIRCUIT.v, clk_divider_nbit.v, FSM_MULT.v
+// 
+// Revision:
+// Revision 1.00 - File Created (11-17-2018) 
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module SR_Muliplier_5b(input CLK,input BTN, input [9:0] SW, output [9:0] SUM);
 
 wire CLK_S, EQ, CLR, LD;
