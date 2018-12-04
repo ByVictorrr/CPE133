b2e924934c15600526d5cc8ac2f1b909ca68cb35
Victor Delaplaine <vdelaplainess@gmail.com>
2018-12-03 20:25:29 -0800

    Update SORT_4b.v

--- lab12/SORT_4b.v
+++ lab12/SORT_4b.v
@@ -1,3 +1,7 @@
+`include "./FSM_4b_SORT.v"
+`include "./SLAVE_CIRCUIT.v"
+`include "../Modules/clk_divider_nbit.v"
+
 `timescale 1ns / 1ps
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -10,7 +14,9 @@
 //// Project Name: Exp 12
 //// Target Devices: Basy3
 //// Tool Versions: 
-///Description: Takes one external input - BTN. When the BTN is pressed the
+///Description: Takes one external input - BTN. When the BTN is pressed the the sort starts, 
+//                the operation that is performed is a 4 4-bit bubble sort. 
+//
 //// Dependencies:  Multiplexed_Display.v
 //// 
 //// Revision:
@@ -20,11 +26,15 @@
 ////////////////////////////////////////////////////////////////////////////////////
 //
 
-`include "./FSM_4b_SORT.v"
-`include "./SLAVE_CIRCUIT.v"
-`include "../Modules/clk_divider_nbit.v"
 
-module SORT_4b(input BTN, input [15:0] SW,  input CLK, output led, output [3:0] arr_0, output [3:0] arr_1, output [3:0] arr_2, output [3:0] arr_3); 
+module SORT_4b(input BTN, 
+               input [15:0] SW,  
+               input CLK, 
+               output led, 
+               output [3:0] arr_0, 
+               output [3:0] arr_1, 
+               output [3:0] arr_2, 
+               output [3:0] arr_3); 
 
 
 wire CLK_SLOWER, CLR;
@@ -33,11 +43,29 @@ wire [3:0] LD;
 wire [5:0] SEL;
 
 clk_divider_nbit #(.n(25)) slowerCLK(.clockin(CLK),.clockout(CLK_SLOWER));
+  
 //controller circuit
-FSM_4b_SORT fsm(.CLK(CLK_SLOWER), .BTN(BTN), .LT(LT), .led(led), .SEL(SEL), .LD(LD), .CLR(CLR)); 
+FSM_4b_SORT fsm(
+    .CLK(CLK_SLOWER), 
+    .BTN(BTN), 
+    .LT(LT), 
+    .led(led), 
+    .SEL(SEL), 
+    .LD(LD), 
+    .CLR(CLR)); 
 
 //slave circuit instantion
-SLAVE_CIRCUIT fsmControlled(.CLK(CLK_SLOWER),.CLR(CLR),.SW(SW),.SEL(SEL), .LD(LD), .LT(LT),.arr_0(arr_0),.arr_1(arr_1),.arr_2(arr_2),.arr_3(arr_3)); 
+SLAVE_CIRCUIT fsmControlled(
+    .CLK(CLK_SLOWER),
+    .CLR(CLR),
+    .SW(SW),
+    .SEL(SEL), 
+    .LD(LD), 
+    .LT(LT),
+    .arr_0(arr_0),
+    .arr_1(arr_1),
+    .arr_2(arr_2),
+    .arr_3(arr_3)); 
 
 
 endmodule
