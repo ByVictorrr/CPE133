c0eadaf7b1ee347347e0cf5d5bea26eb636a1e4f
Victor Delaplaine <vdelaplainess@gmail.com>
2018-12-03 20:09:16 -0800

    Update SLAVE_CIRCUIT.v

--- lab12/SLAVE_CIRCUIT.v
+++ lab12/SLAVE_CIRCUIT.v
@@ -7,11 +7,13 @@
 // Design Name: 
 // Module Name: SLAVE_CIRCUIT
 // Project Name: 
-// Target Devices: 
+// Target Devices: basy3
 // Tool Versions: 
-// Description: 
+// Description: Controlled by the FSM, takes in the Switches as inputs, LD,SEL, and LD signals which are control signals from FSM.
+// The output of this circuit gives the FSM a status for what needs to be selected based on the LT signals of the array of comparators.
+// The four muxes allow the shift the value in the circuit.
 //
-// Dependencies: 
+// Dependencies: mux_2t1_nb.v, mux_4t1_nb.v, comp_nb.v, clk_divider_nbit.v
 // 
 // Revision:
 // Revision 1.00 - File Created (07-07-2018) 
@@ -41,11 +43,25 @@ module SLAVE_CIRCUIT(input CLK, input CLR,  input [15:0] SW, input [5:0]SEL, inp
         
         mux_4t1_nb #(.n(4)) M_middle_0(
 		.SEL({SEL[2],SEL[1]}),
-		.D0({SW[7],SW[6],SW[5],SW[4]}),.D1(arr_0),.D2(arr_2),.D3(arr_2),.D_OUT(M_1));    
+		.D0({SW[7],SW[6],SW[5],SW[4]}),
+		.D1(arr_0),
+		.D2(arr_2),
+		.D3(arr_2),
+		.D_OUT(M_1));    
         
-        mux_4t1_nb #(.n(4)) M_middle_1(.SEL({SEL[4],SEL[3]}),.D0({SW[11], SW[10], SW[9], SW[8]}),.D1(arr_3),.D2(arr_1),.D3(arr_1),.D_OUT(M_2));
+        mux_4t1_nb #(.n(4)) M_middle_1(
+		.SEL({SEL[4],SEL[3]}),
+		.D0({SW[11], SW[10], SW[9], SW[8]}),
+		.D1(arr_3),
+		.D2(arr_1),
+		.D3(arr_1),
+		.D_OUT(M_2));
         
-        mux_2t1_nb #(.n(4)) M_edges_1(.SEL(SEL[5]),.D0({SW[15],SW[14], SW[13], SW[12]}),.D1(arr_2),.D_OUT(M_3));
+        mux_2t1_nb #(.n(4)) M_edges_1(
+		.SEL(SEL[5]),
+		.D0({SW[15],SW[14], SW[13], SW[12]}),.
+		D1(arr_2),
+		.D_OUT(M_3));
 
 	
 	//array of registers
